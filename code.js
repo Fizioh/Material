a209daaa1973d5ee9ab2dc390fba95e364ef1d4a
Benosmane Yassine <54205753+Fizioh@users.noreply.github.com>
2020-07-21 16:01:12 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -37,7 +37,8 @@ Runner.run(runner,engine);
 var stack = Composites.stack(20, 20, 20, 5, 0, 0, function (x, y) {
     return Bodies.circle(x, y, Common.random(10, 20), { friction: 0.00001, restitution: 0.5, density: 0.001 });
 });
-
+ //passer en flase pour ne plus voir les pentes rectangles
+ //passer isStatic en flase pour qu'elles tombent aussi
 World.add (world, stack);
 World.add(world, [
     Bodies.rectangle(250, 150, 700, 5, { isStatic: true, angle: Math.PI * 0.06, render: {visible: true} }),
@@ -45,5 +46,32 @@ World.add(world, [
     Bodies.rectangle(340, 500, 700, 10, {isStatic: true, angle: Math.PI * 0.05, render: { visible: true} })
 ]);
 
+World.add(world, [
+    Bodies.rectangle(400, 600, 1200, 10.5, { isStatic: true})
+]);
+
+var stack = Composites.stack(100, 0, 10, 8, 10, 10, function(x, y) {
+    return Bodies.circle(x, y, Common.random(15, 30), { restitution: 0.6, friction: 0.1 });
+});
+
+World.add(world, [
+    stack,
+    Bodies.polygon(200, 460, 3, 60),
+    Bodies.polygon(400, 460, 5, 60),
+    Bodies.rectangle(600, 460, 80, 80)
+]);
+
+var mouse = Mouse.create(render.canvas),
+mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+        stiffness: 02,
+        render: {
+            visible: false
+        }
+    }
+});
+
+render.mouse = mouse;
 
 Render.lookAt ( render, Composite, allBodies(world));
